6f4bf882f35d0161ffd5cb217858b161f6f0c57f
Jrscott <jrscott@ncsu.edu>
2015-04-12 08:45:35 -0400

    All plots for project 1

---
+++ plot2.R
@@ -0,0 +1,36 @@
+## Exploratory data analysis
+## Project 1
+library(data.table)
+library(lubridate)
+library(datasets)
+filepath = paste("C:/Users/Jason/Dropbox/Coursera/",
+                 "04_ExploratoryAnalysis/data/household_power_consumption.txt",sep="")
+
+
+#import data
+classes=c("character","character","numeric","numeric","numeric","numeric","numeric",
+          "numeric")
+d1<-read.table(filepath,header=TRUE, sep = ";",
+               na.strings="?",colClasses=classes)
+
+
+
+#subset data by date feb 1 2007 and feb 2 2007
+d2<-d1[which(d1$Date=='1/2/2007' | d1$Date=='2/2/2007'),]
+
+#format dates and times as dates and times instead of characters
+d2$Time<-strptime(paste(d2$Date,d2$Time),format='%d/%m/%Y %H:%M:%S',tz="")
+d2$Date<-as.Date(d2$Date,format='%d/%m/%Y')
+
+##plot 2
+png(file="C:/Users/Jason/datasciencecoursera/ExploratoryDataProjects/ExData_Plotting1/plot2.png")
+with(d2,plot(Time,Global_active_power,type="l",
+             xlab="",ylab="Global Active Power (kilowatts)"))
+
+
+#save with width and height of 480 pixels
+png(filename="C:/Users/Jason/datasciencecoursera/ExploratoryDataProjects/ExData_Plotting1/plot2.png",width=480,height=480)
+
+dev.off()
+dev.off()
+
